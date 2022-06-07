1ce6b4091ca7c3c269c0a9973c7fed4b7fbc895a
ijaime <ijaime@mcllent.com>
2022-06-07 09:56:50 -0500

    Creacion dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './views/login/login.component';
 import { InicioComponent } from './views/inicio/inicio.component';
 import { NotFoundComponent } from './views/not-found/not-found.component';
+import { NavComponent } from './common/nav/nav.component';
+import { HeaderComponent } from './common/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     InicioComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    NavComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  iconPassword: string = 'fa fa-eye-slash password-icon';
   constructor() { }
 
   ngOnInit(): void {
+    this.mostrarContrasenia();
+  }
+
+  mostrarContrasenia() {
+    let password = document.getElementById('typepass') as HTMLInputElement;
+    let viewPassword = document.getElementById('viewPassword') as HTMLInputElement;
+    let click = false;
+
+    viewPassword.addEventListener('click', (e)=>{
+      if(!click){
+        password.type = 'text';
+        click = true;
+        this.iconPassword = 'fa fa-eye password-icon';
+      }else if(click){
+        password.type = 'password';
+        click = false;
+        this.iconPassword = 'fa fa-eye-slash password-icon';
+      }
+    })
   }
 
 }
